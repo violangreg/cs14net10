d442ab4b8c5635bcdc784aba2ccd55a014c4f2ba
Greg Violan <violan.greg@gmail.com>
2025-12-23 19:36:50 -0800

    Exercises: Overflow and FizzBuzz

---
+++ Chapter03/Exercise_FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿// FizzBuzz: Write a program that prints the numbers from 1 to max. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+int max = 321;
+for (int i = 1; i <= max; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Write("FizzBuzz");
+    }
+    else if (i % 5 == 0)
+    {
+        Write("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Write("Fizz");
+    }
+    else
+    {
+        Write(i);
+    }
+
+    // put a comma and space after every number except max
+    if (i < max)
+        Write(", ");
+    // new line after every 20 numbers
+    if (i % 20 == 0)
+        WriteLine();
+}
--- Chapter03/Exercise_LoopsAndOverflow/Program.cs
+++ Chapter03/Exercise_LoopsAndOverflow/Program.cs
@@ -1,5 +1,27 @@
 ﻿int max = 500;
-for (byte i = 0; i < max; i++)
+byte i = 0;
+try
 {
-    WriteLine(i);
+    checked
+    {
+        for (i = 0; i < max; i++)
+        {
+            WriteLine(i);
+        }
+    }
 }
+catch (OverflowException)
+{
+    WriteLine("The code overflowed but I caught the exception.");
+    int j = i;
+    for (j++; j < max; j++)
+    {
+        WriteLine(j);
+    }
+}
+catch (Exception ex)
+{
+    WriteLine($"Unexpected exception: {ex.Message}");
+}
+// byte is an 8-bit unsigned integer that can hold values from 0 to 255,
+// it will never reach max because byte overflows at 255 so when it hits 255 it wraps around to 0 again.
