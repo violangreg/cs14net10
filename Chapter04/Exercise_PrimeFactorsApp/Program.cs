5e1f19f813daecb0909343cd9813afddb1ab75a0
Greg Violan <violan.greg@gmail.com>
2026-01-15 21:46:57 -0800

    Exercise_PrimeFactors, Instrumenting (Logging)

---
+++ Chapter04/Exercise_PrimeFactorsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercise_PrimeFactorsLib;
+
+Console.WriteLine("Enter a number between 1 and 1000 to factor (or 'exit' to quit):");
+var input = Console.ReadLine();
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine(
+        format: "Prime factors of {0} are: {1}",
+        arg0: number,
+        arg1: PrimeFactorsLib.PrimeFactors(number)
+    );
+}
---
+++ Chapter04/Exercise_PrimeFactorsTests/PrimeFactorsTests.cs
@@ -0,0 +1,26 @@
+﻿using Exercise_PrimeFactorsLib;
+
+namespace Exercise_PrimeFactorsTests;
+
+public class PrimeFactorsTests
+{
+    [Theory]
+    [InlineData(-112, "Number -112 is out of range (1-1000)")]
+    [InlineData(0, "Number 0 is out of range (1-1000)")]
+    [InlineData(1, "None")]
+    [InlineData(2, "2")]
+    [InlineData(4, "2 x 2")]
+    [InlineData(90, "5 x 3 x 3 x 2")]
+    [InlineData(330, "11 x 5 x 3 x 2")]
+    [InlineData(987, "47 x 7 x 3")]
+    [InlineData(1111, "Number 1111 is out of range (1-1000)")]
+    public void TestPrimeFactors(int number, string expected)
+    {
+        // Arrange + Act - call PrimeFactors method
+        string actual = PrimeFactorsLib.PrimeFactors(number);
+        // Assert - verify the result
+        Assert.Equal(expected, actual);
+    }
+}
+
+// build to run tests
