607398966e0c3e6761470f2bdbd283584c89b4a5
Greg Violan <violan.greg@gmail.com>
2026-03-27 20:36:32 -0700

    End of Ch 13

--- ModernWeb/Northwind.Web/Program.cs
+++ ModernWeb/Northwind.Web/Program.cs
@@ -1,6 +1,115 @@
+// To use StaticWebAssetsLoader.
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.AspNetCore.Server.Kestrel.Core; // To use HttpProtocols.
+using Northwind.EntityModels; // To use AddNorthwindContext method.
+using Northwind.Web.Components; // To use App.
+#region Configure the web server host and services.
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorComponents();
+builder.Services.AddNorthwindContext();
+builder.Services.AddRequestDecompression();
+
+// Only necessary if you want to switch environments during development.
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+builder.WebHost.ConfigureKestrel(
+    (context, options) =>
+    {
+        options.ConfigureEndpointDefaults(listenOptions =>
+        {
+            listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+            listenOptions.UseHttps(); // HTTP/3 requires secure connections.
+        });
+    }
+);
 var app = builder.Build();
+#endregion
+#region Configure the HTTP pipeline and routes
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
 
-app.MapGet("/", () => "Hello World!");
+// Implementing an anonymous inline delegate as middleware
+// to intercept HTTP requests and responses.
+app.Use(
+    async (HttpContext context, Func<Task> next) =>
+    {
+        RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
 
-app.Run();
+        if (rep is not null)
+        {
+            WriteLine($"Endpoint name: {rep.DisplayName}");
+            WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+        }
+
+        if (context.Request.Path == "/bonjour")
+        {
+            // In the case of a match on URL path, this becomes a terminating
+            // delegate that returns so does not call the next delegate.
+            await context.Response.WriteAsync("Bonjour Monde!");
+            return;
+        }
+
+        // We could modify the request before calling the next delegate.
+        await next();
+
+        // We could modify the response after calling the next delegate.
+    }
+);
+
+app.UseHttpsRedirection();
+app.UseRequestDecompression();
+app.UseAntiforgery();
+#endregion
+app.UseDefaultFiles(); // index.html, default.html, and so on.
+
+//app.UseStaticFiles(); // .NET 8 or earlier.
+
+app.MapStaticAssets(); // .NET 9 or latier.
+app.MapRazorComponents<App>();
+app.MapGet("/env", () => $"Environment is {app.Environment.EnvironmentName}");
+
+app.MapGet(
+    "/data",
+    () =>
+        Results.Json(
+            new
+            {
+                firstName = "John",
+                lastName = "Doe",
+                age = 30,
+            }
+        )
+);
+app.MapGet(
+    "/welcome",
+    () =>
+        Results.Content(
+            content: $"""
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="site.css" rel="stylesheet" />
+  <title>About Northwind Web</title>
+</head>
+<body>
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-3">About Northwind Web</h1>
+      <p class="lead">We supply products to our customers.</p>
+      <img src="categories.jpeg" style="height:200px;width:300px;" />
+    </div>
+  </div>
+</body>
+</html>
+""",
+            contentType: "text/html"
+        )
+);
+
+// Start the web server, host the website, and wait for requests.
+app.Run(); // This is a thread-blocking call.
+WriteLine("This executes after the web server has stopped!");
